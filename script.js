120d0bf39cbcf79b2b0c3b3994d8cbce2ef6ce4b
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 16:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const line = document.getElementById("line");
+line.style.width = "200px";
+line.style.height ="2px";
+line.style.backgroundColor ="black";
+line.style.margin ="auto";
+line.style.marginTop ="25%";
+
+line.style.animation ="rotate 4s linear infinite"
